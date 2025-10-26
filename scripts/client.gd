e4e375f6d2597e627ea89b291999713cd4fc2192
cpiod <cpiod@pm.me>
2025-10-26 11:37:04 +0100

    Ongoing work

--- scripts/client.gd
+++ scripts/client.gd
@@ -3,12 +3,16 @@ extends Node2D
 @export var wanted_food: String
 @export var wanted_other: String
 @export var timeline: String
+@export var sprites: SpriteFrames
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Sprite.sprite_frames = sprites
+	$Sprite.play()
 
+func set_flip() -> void:
+	$Sprite.set_flip_h(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
--- scripts/comptoir.gd
+++ scripts/comptoir.gd
@@ -17,27 +17,63 @@ var can_play = true
 var day: int = -1
 var tries: int
 var client: Node
+var end: bool = false
 
+# ordre des clients
+var clients: Array
+var satisfied: Array = [false, false, false, false]
 
-
-func start_dialog(s: String, label: String = ""):
+func start_dialog(label: String = ""):
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	can_play = false
-	Dialogic.start(s, label)
+	Dialogic.start(client.timeline, label)
 	
 func start_new_day():
+	var tween = get_tree().create_tween()
+	var tween_item = get_tree().create_tween().set_parallel()
+
+	if selected_food_item:
+		tween_item.tween_property(selected_food_item, "position:x", selected_food_item_initial_pos.x, 1).set_trans(Tween.TRANS_SINE)
+		tween_item.tween_property(selected_food_item, "position:y", selected_food_item_initial_pos.y, 1).set_trans(Tween.TRANS_SINE)
+#		selected_food_item.position = selected_food_item_initial_pos
+		selected_food_item = null
+	if selected_other_item:
+		tween_item.tween_property(selected_other_item, "position:x", selected_other_item_initial_pos.x, 1).set_trans(Tween.TRANS_SINE)
+		tween_item.tween_property(selected_other_item, "position:y", selected_other_item_initial_pos.y, 1).set_trans(Tween.TRANS_SINE)
+#		selected_other_item.position = selected_other_item_initial_pos
+		selected_other_item = null
 	day += 1
-	if day == 0:
-		client = $Fantome
+	if client:
+		tween.tween_property(client, "position:x", -450, 1)
+#		tween.tween_property(client, "position:x", -450, 1) # wait a second
+	client = clients[day]
+#	for c in clients:
+#		c.visible = false
+#	client.visible = true
+	tween.tween_property(client, "position:x", -150, 1).set_delay(1.0)
+	tween.tween_callback(start_dialog)#.set_delay(0.5)
 	for c in $Items.get_children():
-		c.visible = c.day <= day
+		if c.day == day:
+			var old_pos = c.position
+			var distance = sqrt(c.position.x ** 2 + c.position.y ** 2)
+			c.position.x = c.position.x / distance * 500
+			c.position.y = c.position.y / distance * 500
+			tween_item.tween_property(c, "position:x", old_pos.x, 1).set_trans(Tween.TRANS_SINE)
+			tween_item.tween_property(c, "position:y", old_pos.y, 1).set_trans(Tween.TRANS_SINE)
+			c.visible = true
+		elif c.day > day:
+			c.visible = false
+#		c.visible = c.day <= day
 	tries = 0
-	start_dialog(client.timeline)
+#	start_dialog()
 
 func _on_timeline_ended():
 	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
 	can_play = true
-	# do something else here
+	if end:
+		client.set_flip()
+		start_new_day()
+		end = false
 
 func _process(_delta: float) -> void:
 	if selected_item:
@@ -53,6 +89,7 @@ func is_over_drop_zone() -> bool:
 	return space_state.intersect_point(parameters).size() > 0
 
 func _input(event):
+	var tween = get_tree().create_tween().set_parallel()
 	# left click
 	# only possible during player turn
 	if can_play and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -69,17 +106,21 @@ func _input(event):
 					# l’item n’a peut-être pas bougé de la zone de dépose
 					if selected_food_item != selected_item:
 						if selected_food_item: # range l’item précédent
-							selected_food_item.position = selected_food_item_initial_pos
+#							selected_food_item.position = selected_food_item_initial_pos
+							tween.tween_property(selected_food_item, "position", selected_food_item_initial_pos, 0.2)
 						selected_food_item = selected_item
 						selected_food_item_initial_pos = initial_item_pos
-					selected_item.position = $"Dépose/FoodItemPos".position
+					tween.tween_property(selected_item, "position", $"Dépose/FoodItemPos".position, 0.1)
+#					selected_item.position = $"Dépose/FoodItemPos".position
 				else:
 					if selected_other_item != selected_item:
 						if selected_other_item: # range l’item précédent
-							selected_other_item.position = selected_other_item_initial_pos
+#							selected_other_item.position = selected_other_item_initial_pos
+							tween.tween_property(selected_other_item, "position", selected_other_item_initial_pos, 0.2)
 						selected_other_item = selected_item
 						selected_other_item_initial_pos = initial_item_pos
-					selected_item.position = $"Dépose/OtherItemPos".position
+					tween.tween_property(selected_item, "position", $"Dépose/OtherItemPos".position, 0.1)
+#					selected_item.position = $"Dépose/OtherItemPos".position
 				if selected_food_item and selected_other_item:
 					$"Dépose/BoutonValider".disabled = false
 			else:
@@ -91,7 +132,8 @@ func _input(event):
 					initial_item_pos = selected_other_item_initial_pos
 					selected_other_item = null
 					$"Dépose/BoutonValider".disabled = true
-				selected_item.position = initial_item_pos
+#				selected_item.position = initial_item_pos
+				tween.tween_property(selected_item, "position", initial_item_pos, 0.1)
 			selected_item = null
 
 func find_item() -> Node:
@@ -107,19 +149,25 @@ func find_item() -> Node:
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	clients = [$Fantome, $Loup, $Fée, $Cyclope]
 	start_new_day()
 
 
 func _on_texture_button_pressed() -> void:
+	end = false
 	var edible_correct = selected_food_item.title == client.wanted_food
 	var other_correct = selected_other_item.title == client.wanted_other
-	print(edible_correct, other_correct)
-	if !edible_correct and !other_correct:
-		start_dialog("fantome", "0-correct")
-	elif edible_correct and other_correct:
-		start_dialog("fantome", "2-correct")
+	if edible_correct and other_correct:
+		satisfied[day] = true
+		end = true
+		start_dialog("2-correct")
+	elif tries == 1:
+		end = true
+		start_dialog("2nd-error")
+	elif !edible_correct and !other_correct:
+		start_dialog("0-correct")
 	elif edible_correct:
-		start_dialog("fantome", "wrong-other")
+		start_dialog("wrong-other")
 	elif other_correct:
-		start_dialog("fantome", "wrong-edible")
+		start_dialog("wrong-edible")
 	tries += 1
